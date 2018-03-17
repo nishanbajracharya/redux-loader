158ad48e2b17632edba9781ac2b6b96d63d4903f
Nishan Bajracharya <nishanbajracharya@lftechnology.com>
2018-03-17 14:36:15 +0545

    Add actions

---
+++ src/action.js
@@ -0,0 +1,34 @@
+export const STOP_LOADING = '@@STOP_LOADING';
+export const START_LOADING = '@@START_LOADING';
+export const REGISTER_LOADER = '@@REGISTER_LOADER';
+export const UNREGISTER_LOADER = '@@UNREGISTER_LOADER';
+
+export const stopLoading = ids => ({
+  type: STOP_LOADING,
+  payload: ids
+});
+
+export const startLoading = ids => ({
+  type: START_LOADING,
+  payload: ids
+});
+
+export const registerLoader = ({
+  id,
+  startAction,
+  successAction,
+  failureAction
+}) => ({
+  type: REGISTER_LOADER,
+  payload: {
+    id,
+    startAction,
+    successAction,
+    failureAction
+  }
+});
+
+export const unregisterLoader = id => ({
+  type: UNREGISTER_LOADER,
+  payload: id
+})
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import reduxLoaderReducer from './reducer';
+import * as reduxLoaderActions from 'action';
+import reduxLoaderMiddleware from './middleware';
+
+export { reduxLoaderActions, reduxLoaderReducer, reduxLoaderMiddleware };
+
+export default reduxLoaderMiddleware;
